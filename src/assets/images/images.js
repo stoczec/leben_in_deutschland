389662fee323a4f5498f5d1edf7d588a118c1934
Dmytro Herashchenko <stoczec@gmail.com>
2026-06-16 09:04:20 +0200

    chore: replace decorative integral images with generated illustrations
    - 56 questions whose image is decorative (text-answerable) get AI flat
      illustrations in one consistent style, replacing substituted press
      photos / party logos / map screenshots; source JPGs deleted
    - 7 genuine exam image-option questions (coat-of-arms, flag, ballot and
      map choices) keep their official images

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -1,129 +1,17 @@
 import img21 from './21.jpg';
-import img22 from './22.jpg';
-import img24 from './24.jpg';
-import img25 from './25.jpg';
-import img27 from './27.jpg';
-import img29 from './29.jpg';
-import img37 from './37.jpg';
-import img38 from './38.jpg';
-import img39 from './39.jpg';
-import img41 from './41.jpg';
-import img49 from './49.jpg';
-import img54 from './54.jpg';
-import img57 from './57.jpg';
-import img64 from './64.jpg';
-import img69 from './69.jpg';
-import img89 from './89.jpg';
-import img92 from './92.jpg';
-import img94 from './94.jpg';
-import img96 from './96.jpg';
-import img98 from './98.jpg';
-import img112 from './112.jpg';
-import img113 from './113.jpg';
-import img114 from './114.jpg';
-import img116 from './116.jpg';
-import img117 from './117.jpg';
-import img118 from './118.jpg';
-import img122 from './122.jpg';
 import img130 from './130.jpg';
-import img143 from './143.jpg';
-import img145 from './145.jpg';
-import img151 from './151.jpg';
-import img167 from './167.jpg';
-import img173 from './173.jpg';
-import img174 from './174.jpg';
-import img175 from './175.jpg';
 import img176 from './176.jpg';
-import img177 from './177.jpg';
-import img185 from './185.jpg';
-import img187 from './187.jpg';
-import img197 from './197.jpg';
-import img198 from './198.jpg';
-import img200 from './200.jpg';
-import img201 from './201.jpg';
-import img202 from './202.jpg';
-import img204 from './204.jpg';
-import img207 from './207.jpg';
 import img209 from './209.jpg';
-import img212 from './212.jpg';
-import img218 from './218.jpg';
-import img224 from './224.jpg';
 import img226 from './226.jpg';
-import img229 from './229.jpg';
-import img238 from './238.jpg';
-import img246 from './246.jpg';
-import img268 from './268.jpg';
-import img279 from './279.jpg';
-import img297 from './297.jpg';
-import img300 from './300.jpg';
 import img301 from './301.jpg';
-import img302 from './302.jpg';
-import img305 from './305.jpg';
-import img306 from './306.jpg';
 import img308 from './308.jpg';
 
 export {
   img21,
-  img22,
-  img24,
-  img25,
-  img27,
-  img29,
-  img37,
-  img38,
-  img39,
-  img41,
-  img49,
-  img54,
-  img57,
-  img64,
-  img69,
-  img89,
-  img92,
-  img94,
-  img96,
-  img98,
-  img112,
-  img113,
-  img114,
-  img116,
-  img117,
-  img118,
-  img122,
   img130,
-  img143,
-  img145,
-  img151,
-  img167,
-  img173,
-  img174,
-  img175,
   img176,
-  img177,
-  img185,
-  img187,
-  img197,
-  img198,
-  img200,
-  img201,
-  img202,
-  img204,
-  img207,
   img209,
-  img212,
-  img218,
-  img224,
   img226,
-  img229,
-  img238,
-  img246,
-  img268,
-  img279,
-  img297,
-  img300,
   img301,
-  img302,
-  img305,
-  img306,
   img308,
 };
